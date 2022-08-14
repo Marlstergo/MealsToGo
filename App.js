501e27f0e4599a19571e0e52743e6db603312cf1
marlstergo <abdulmaliqshola@gmail.com>
2022-08-14 11:10:50 +0100

    myapp

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
-import {
-  // SafeAreaView,
-  // StyleSheet,
-  // Text,
-  // View,
-  // Platform,
-  StatusBar,
-  React,
-} from "react-native";
-import RestaurantScreen from "./src/features/restaurant/screens/RestaurantScreen";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { ThemeProvider } from "styled-components";
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 export default function App() {
-  console.log(StatusBar.currentHeight);
+  // console.log(StatusBar.currentHeight);
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
     <>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
